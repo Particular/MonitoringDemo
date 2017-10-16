8c8cbc7313b9b7d14d2b11bcdb4de90226591445
Ramon Smits <ramon.smits@gmail.com>
2017-10-16 22:39:26 +0200

    Added '-Verb runas' to application to launch them elevated due to port bindings which needs admin priviledge

--- MonitoringDemo.Sql/RunPlatform.ps1
+++ MonitoringDemo.Sql/RunPlatform.ps1
@@ -1,11 +1,11 @@
 Write-Host "Starting ServiceControl instance"
-Start-Process ".\Platform\servicecontrol\servicecontrol-instance\bin\ServiceControl.exe" -WorkingDirectory ".\Platform\servicecontrol\servicecontrol-instance\bin"
+Start-Process ".\Platform\servicecontrol\servicecontrol-instance\bin\ServiceControl.exe" -WorkingDirectory ".\Platform\servicecontrol\servicecontrol-instance\bin" -Verb runas
 
 Write-Host "Starting Monitoring instance"
-Start-Process ".\Platform\servicecontrol\monitoring-instance\ServiceControl.Monitoring.exe" -WorkingDirectory ".\Platform\servicecontrol\monitoring-instance"
+Start-Process ".\Platform\servicecontrol\monitoring-instance\ServiceControl.Monitoring.exe" -WorkingDirectory ".\Platform\servicecontrol\monitoring-instance" -Verb runas
 
 Write-Host "Wait for SC instances to have fully started"
 Start-Sleep -s 5
 
 Write-Host "Starting ServicePulse"
-Start-Process ".\Platform\servicepulse\ServicePulse.Host.exe" -WorkingDirectory ".\Platform\servicepulse"
+Start-Process ".\Platform\servicepulse\ServicePulse.Host.exe" -WorkingDirectory ".\Platform\servicepulse" -Verb runas
