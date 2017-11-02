4900a820a7c4914718b6b7f14956ad05524ca663
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-11-02 15:15:50 +0100

    creating database in a separate PS process

--- MonitoringDemo.Sql/support/Utils.psm1
+++ MonitoringDemo.Sql/support/Utils.psm1
@@ -1,36 +1,4 @@
-﻿[Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
-
-Function New-Database
-{
-    param(
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string] $server,
-
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string] $databaseName
-    )
-
-    $srv = New-Object Microsoft.SqlServer.Management.Smo.Server($server)
-
-    # Check can connect
-    $srv.ConnectionContext.Connect()
-
-    if ($srv.Databases[$databaseName] -ne $Null)
-    {
-        Write-Host "Dropping" + $databaseName
-        $srv.KillAllProcesses($databaseName)
-        $srv.KillDatabase($databaseName)
-    }
-
-    $db = New-Object Microsoft.SqlServer.Management.Smo.Database($srv, $databaseName)
-    
-    # TODO: Do we need to explicitly set credentials and create the schema? 
-    $db.Create()
-}
-
-Function New-ConnectionString {
+﻿Function New-ConnectionString {
     param(
         [Parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
@@ -59,44 +27,6 @@ Function New-ConnectionString {
     {
         return "server=$($server);Database=$($databaseName);Uid=$($uid);Pwd=$($pwd)"
     }
-}
-
-Function Install-Msi {
-    param (
-        [System.IO.FileInfo]$file
-    )
-    $DataStamp = get-date -Format yyyyMMddTHHmmss
-    $logFile = '{0}-{1}.log' -f $file.fullname,$DataStamp
-    $MSIArguments = @(
-        "/i"
-        ('"{0}"' -f $file.fullname)
-        "/qn"
-        "/norestart"
-        "/L*v"
-        $logFile
-        "IACCEPTSQLLOCALDBLICENSETERMS=YES"
-    )
-    
-    $process = Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow -PassThru
-    
-    if($process.ExitCode -ne 0){
-        Write-Error -Message "Failed installing ($file.Path)"
-        Read-Host
-    }
-    
-}
-
-Function Add-LocalDbInstance {
-    param (
-        [string]$instanceName
-    )
-    
-    sqllocaldb create $instanceName | Out-Null
-    sqllocaldb share $instanceName $instanceName | Out-Null
-    sqllocaldb start $instanceName | Out-Null
-    $info = sqllocaldb info $instanceName
-
-    return $info.Split(" ") | where{$_ -like "np:\\.\pipe*"}
 }
 
 Function Update-ConnectionStrings {
@@ -156,4 +86,17 @@ Function Test-SQLConnection
         
         exit
     }
+}
+
+Function Add-LocalDbInstance {
+    param (
+        [string]$instanceName
+    )
+    
+    sqllocaldb create $instanceName | Out-Null
+    sqllocaldb share $instanceName $instanceName | Out-Null
+    sqllocaldb start $instanceName | Out-Null
+    $info = sqllocaldb info $instanceName
+
+    return $info.Split(" ") | where{$_ -like "np:\\.\pipe*"}
 }
