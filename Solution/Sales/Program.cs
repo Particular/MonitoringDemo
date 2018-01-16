6c42013f20c463a1cb7efb74ce053cc5367d05b5
Tomasz Masternak <tomasz.masternak@gmail.com>
2018-01-16 09:18:27 +0100

    deterministic ids for all sales instances

--- Solution/Sales/Program.cs
+++ Solution/Sales/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using NServiceBus;
 using Shared;
@@ -15,21 +17,19 @@ namespace Sales
             LoggingUtils.ConfigureLogging("Sales");
 
             var instanceName = args.FirstOrDefault();
-            var instanceId = Guid.NewGuid();
 
             if(string.IsNullOrEmpty(instanceName))
             {
                 Console.Title = "Sales";
 
                 instanceName = "original-instance";
-                instanceId = new Guid("14A46D23-4874-497B-ABCD-8D6E2488DB25");
             }
             else
             {
                 Console.Title = $"Sales - {instanceName}";
             }
 
-            Console.WriteLine("Using instance-id {0}", instanceName);
+            var instanceId = DeterministicGuid.Create("Sales", instanceName);
 
             var endpointConfiguration = new EndpointConfiguration("Sales");
             endpointConfiguration.LimitMessageProcessingConcurrencyTo(4);
@@ -91,6 +91,21 @@ namespace Sales
                     case ConsoleKey.Escape:
                         return;
                 }
+            }
+        }
+    }
+
+    static class DeterministicGuid
+    {
+        public static Guid Create(params object[] data)
+        {
+            // use MD5 hash to get a 16-byte hash of the string
+            using (var provider = new MD5CryptoServiceProvider())
+            {
+                var inputBytes = Encoding.Default.GetBytes(string.Concat(data));
+                var hashBytes = provider.ComputeHash(inputBytes);
+                // generate a guid from the hash:
+                return new Guid(hashBytes);
             }
         }
     }
