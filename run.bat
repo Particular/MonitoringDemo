306703059b4296aaf84b1207095121375323f4ab
danielmarbach <daniel.marbach@openplace.net>
2018-11-27 11:46:31 +0100

    Run powershell elevated

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0\Run.ps1""' }"
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0\Run.ps1""' -Verb RunAs }"
