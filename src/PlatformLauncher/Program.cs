a5ab013b6c731b8fa53a76a0a6df557a3d7ef312
Brandon Ording <bording@gmail.com>
2025-03-19 14:38:00 -0400

    Rename Platform project

---
+++ src/PlatformLauncher/Program.cs
@@ -0,0 +1,12 @@
+﻿using Particular;
+
+Console.Title = "PlatformLauncher";
+try
+{
+    await PlatformLauncher.Launch(showPlatformToolConsoleOutput: false, servicePulseDefaultRoute: "/monitoring");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    Console.ReadLine();
+}
