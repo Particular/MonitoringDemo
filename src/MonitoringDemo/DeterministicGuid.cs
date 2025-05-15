c48b7235e2010e4b8b383cf54f31b246c7def71d
Brandon Ording <bording@gmail.com>
2025-05-15 17:19:06 -0400

    Tweaks

--- src/MonitoringDemo/DeterministicGuid.cs
+++ src/MonitoringDemo/DeterministicGuid.cs
@@ -5,11 +5,9 @@ static class DeterministicGuid
 {
     public static Guid Create(params object[] data)
     {
-        // use MD5 hash to get a 16-byte hash of the string
-        using var provider = MD5.Create();
         var inputBytes = Encoding.Default.GetBytes(string.Concat(data));
-        var hashBytes = provider.ComputeHash(inputBytes);
-        // generate a guid from the hash:
+        var hashBytes = MD5.HashData(inputBytes);
+
         return new Guid(hashBytes);
     }
 }
