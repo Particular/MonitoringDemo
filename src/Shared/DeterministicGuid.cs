c352dace3dcb9681fc5a89734fc8835a19ecb2b0
SzymonPobiega <szymon.pobiega@gmail.com>
2025-05-16 13:23:41 +0200

    Fix rebase

--- src/Shared/DeterministicGuid.cs
+++ src/Shared/DeterministicGuid.cs
@@ -1,7 +1,7 @@
 using System.Security.Cryptography;
 using System.Text;
 
-static class DeterministicGuid
+public static class DeterministicGuid
 {
     public static Guid Create(params object[] data)
     {
